429c648c49a90e752218ccc2fb1ee567843aa9b7
David Edward Weirich <dweirich@pillartechnology.com>
2016-05-19 15:57:49 -0400

    Working with 5s

--- src/main/scala/fizzbuzz.scala
+++ src/main/scala/fizzbuzz.scala
@@ -2,9 +2,13 @@ package fizzbuzz
 
 object FizzBuzzer{
 
-  def apply (input:Int) : String = (input % 3) match{
-    case 0 => "fizz"
-    case _ => input
-    return "" + input
+  def apply (input:Int) : String = (input % 15) match {
+    case 3 => "fizz"
+    case 6 => "fizz"
+    case 9 => "fizz"
+    case 12 => "fizz"
+    case 5 => "buzz"
+    case 10 => "buzz"
+    case _ => "" + input
   }
 }
